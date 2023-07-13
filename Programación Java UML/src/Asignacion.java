1eda137297290b1db90d6dcb2e02d83c691c0090
Abel214 <131829158+Abel214@users.noreply.github.com>
2023-07-12 19:38:52 -0500

    Create Asignacion.java

---
+++ Programación Java UML/src/Asignacion.java
@@ -0,0 +1,10 @@
+public class Asignacion {
+    private Temporada temporada;
+    private Modalidad modalidad;
+    private Partido partido;
+    private boolean idaYVueltas;
+    public void generarFechaPartido(){
+        // generar fecha del partido
+    }
+
+}
