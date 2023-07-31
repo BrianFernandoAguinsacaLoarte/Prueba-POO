8baeadbb515a4e62cde17a8f4167db1c191d6087
Abel214 <131829158+Abel214@users.noreply.github.com>
2023-07-31 17:48:55 -0500

    Archivos solo en tercera unidad

--- Proyecto Final/src/EstadoInscripcion.java
+++
@@ -1,9 +0,0 @@
-public enum EstadoInscripcion {
-    PENDIENTE,
-    APROBADA,
-    RECHAZADA;
-
-    public static EstadoInscripcion getRandomEstadoInscripcion() {
-        return values()[(int) (Math.random() * values().length)];
-    }
-}
