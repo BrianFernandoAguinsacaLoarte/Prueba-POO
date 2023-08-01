2c1d565351cd6cad635f156aafdb1628b9f66e48
Abel214 <131829158+Abel214@users.noreply.github.com>
2023-07-31 19:05:11 -0500

    Update EleccionDeRol.java
    Se cambia asignación o se actualiza por una clase denominada Elección de rol como enumerador

--- Programación Java UML/src/Asignacion.java
+++ Programación Java UML/src/Asignacion.java
@@ -1,10 +1,5 @@
-public class Asignacion {
-    private Temporada temporada;
-    private Modalidad modalidad;
-    private Partido partido;
-    private boolean idaYVueltas;
-    public void generarFechaPartido(){
-        // generar fecha del partido
-    }
-
+public enum EleccionDeRol {
+    JUGADOR,
+    DIRECTOR_TECNICO,
+    ARBITRO,
 }
