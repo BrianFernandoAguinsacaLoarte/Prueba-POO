36d37eb4287f62b02cf0a85d525532046df8c6bc
Brian Fernando Aguinsaca Loarte <133794609+BrianFernandoAguinsacaLoarte@users.noreply.github.com>
2023-08-10 10:01:38 -0500

    Modificación de los settings

--- Código Python/DawnFM/settings.py
+++ Código Python/DawnFM/settings.py
@@ -117,9 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'dawnbem/static'),
-]
+STATICFILES_DIRS = ['C:/Users/Usuario iTC/Documents/Tareas/2 ciclo/Programacion/Pycharm/DawnFM/dawnbem/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
