a3d53b408340361fbb3a3159ec6d9f482b81472e
Abel214 <131829158+Abel214@users.noreply.github.com>
2023-08-12 10:42:35 -0500

    Update Grupo.java
    Actualización

--- PruebaFinal 2.0/src/Grupo.java
+++ PruebaFinal 2.0/src/Grupo.java
@@ -1,118 +1,63 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Grupo {
+    private List<String> equiposEnGrupo = new ArrayList<>();
     private String nombre;
-    private int capacidadMaxima;
-    private List<Equipo> equipos;
-    private Equipo equipoLocal;
-    private Equipo equipoVisitante;
-    private List<Partido> listaPartidos = new ArrayList<>();
+    private List<Partido> partidos;
+    private static final int LIMITE_EQUIPOS = 4;
 
-    public List<Partido> getListaPartidos() {
-        return listaPartidos;
+
+    public Grupo() {
+            this.nombre = "Grupo ";
+    }
+    public void agregarEquipo(String nombreEquipo) {
+        equiposEnGrupo.add(nombreEquipo);
     }
 
-    public void setListaPartidos(List<Partido> listaPartidos) {
-        this.listaPartidos = listaPartidos;
+    public List<String> getEquiposEnGrupo() {
+        return equiposEnGrupo;
     }
 
-    public Equipo getEquipoLocal() {
-        return equipoLocal;
-    }
+    public static List<Grupo> definirGrupos(String[] equipos) {
+        List<Grupo> gruposDefinidos = new ArrayList<>();
+        Grupo grupoActual = new Grupo();
 
-    public void setEquipoLocal(Equipo equipoLocal) {
-        this.equipoLocal = equipoLocal;
-    }
-
-    public Equipo getEquipoVisitante() {
-        return equipoVisitante;
-    }
-
-    public void setEquipoVisitante(Equipo equipoVisitante) {
-        this.equipoVisitante = equipoVisitante;
-    }
-
-    public Grupo(String nombre, int capacidadMaxima, List<Equipo> equiposInscritos) {
-        this.nombre = nombre;
-        this.capacidadMaxima = capacidadMaxima;
-        this.equipos = equiposInscritos != null ? new ArrayList<>(equiposInscritos) : new ArrayList<>();
-
-    }
-
-    public List<Equipo> getEquipos() {
-        return equipos;
-    }
-
-    public void generarPartidosYCrearGrupos(List<Grupo> grupos) {
-        for (int i = 0; i < equipos.size(); i++) {
-            Equipo equipo = equipos.get(i);
-            if (agregarEquipo(equipo)) {
-                equipos.remove(i);
-                i--; // Decrementar el índice para evitar saltarse un equipo al eliminarlo de la lista
+        for (String equipo : equipos) {
+            if (!grupoActual.puedeAgregarEquipo()) {
+                gruposDefinidos.add(grupoActual);
+                grupoActual = new Grupo();
             }
+            grupoActual.agregarEquipo(equipo);
         }
 
-
-        if (equipos.size() >= 2) {
-            System.out.println("¡EN HORA BUENA QUE COMIENCEN LOS PARTIDOS!");
-
-            while (equipos.size() >= 2) {
-                Partido partido = new Partido();
-                String nombrePartidoAleatorio = partido.generarNombreAleatorio();
-                System.out.println("Nombre del Partido: " + nombrePartidoAleatorio);
-
-                EstadoPartido estadoPartido = EstadoPartido.generarEstadoPartido();
-                System.out.println("Estado del Partido: " + estadoPartido);
-
-                partido.setEquipoLocal(equipoLocal);
-                partido.setEquipoVisitante(equipoVisitante);
-                listaPartidos.add(partido);
-
-                definirEquiposLocalesYVisitantes(); // Actualizar equipos locales y visitantes para el siguiente partido
-            }
-        } else {
-            System.out.println("Se necesitan al menos 2 equipos para definir los equipos locales y visitantes.");
-            System.out.println("En unos momentos se definirán los partidos.");
+        // Agregar el último grupo si no está lleno
+        if (!grupoActual.getEquiposEnGrupo().isEmpty()) {
+            gruposDefinidos.add(grupoActual);
         }
 
-        if (equipos.size() > 1) {
-            grupos.add(this);
-        } else {
-            System.out.println("No se pudo formar un grupo con el equipo actual. Se necesita al menos otro equipo para formar un grupo.");
-        }
+        return gruposDefinidos;
+    }
+    public boolean puedeAgregarEquipo() {
+        return equiposEnGrupo.size() < LIMITE_EQUIPOS;
 
     }
+    public List<Partido> generarPartidos() {
+        List<String> equipos = getEquiposEnGrupo();
+        List<String> nombresPartidos = Partido.generarPartidosAleatorios(equipos);
+        List<Partido> partidos = new ArrayList<>();
 
-    public boolean agregarEquipo(Equipo equipo) {
-        if (equipos.size() < capacidadMaxima) {
-            equipos.add(equipo);
-            return true;
-        } else {
-            System.out.println("No se pudo agregar el equipo " + equipo.getNombre() + " porque el grupo ya está lleno.");
-            return false;
+        for (String nombrePartido : nombresPartidos) {
+            String[] equiposPartido = nombrePartido.split(" Vs ");
+            String equipoLocal = equiposPartido[0];
+            String equipoVisitante = equiposPartido[1];
+            Partido partido = new Partido(equipoLocal, equipoVisitante, Partido.generarNombreAleatorio());
+            partidos.add(partido);
         }
-    }
 
-    public void definirEquiposLocalesYVisitantes() {
-        if (equipos.size() >= 2) {
-            Random random = new Random();
-            int indiceLocal = random.nextInt(equipos.size());
-            equipoLocal = equipos.get(indiceLocal);
-
-            int indiceVisitante;
-            do {
-                indiceVisitante = random.nextInt(equipos.size());
-            } while (indiceVisitante == indiceLocal);
-
-            equipoVisitante = equipos.get(indiceVisitante);
-        } else {
-            System.out.println("Se necesitan al menos 2 equipos para definir los equipos locales y visitantes.");
-        }
+        return partidos;
     }
 
 
-    public void agregarPartido(Partido partido) {
-    }
 }
+
