e99519cde3edaebcff1752030f65f8451447bbbd
Abel214 <131829158+Abel214@users.noreply.github.com>
2023-08-12 10:43:07 -0500

    Update Sorteo.java
    Actualiación

--- PruebaFinal 2.0/src/Sorteo.java
+++ PruebaFinal 2.0/src/Sorteo.java
@@ -1,23 +1,13 @@
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class Sorteo {
+class Sorteo {
     private List<Grupo> grupos;
 
-    public Sorteo() {
-        grupos = new ArrayList<>();
+    public Sorteo(List<Grupo> grupos) {
+        this.grupos = grupos;
     }
 
-    public void agregarGrupos(List<Grupo> grupos) {
-        this.grupos.addAll(grupos);
-    }
 
-    public void realizarSorteo() {
-        // Simplemente, definimos equipos locales y visitantes para cada grupo
-        for (Grupo grupo : grupos) {
-            grupo.definirEquiposLocalesYVisitantes();
-        }
-    }
-
-    // Métodos para asignar fechas y horarios (no mostrados para mayor brevedad)
 }
+
