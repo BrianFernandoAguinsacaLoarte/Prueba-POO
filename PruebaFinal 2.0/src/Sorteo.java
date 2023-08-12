915b2deea3c945030744f111ecead6fa395e981c
Abel214 <131829158+Abel214@users.noreply.github.com>
2023-08-12 16:25:30 -0500

    Update Sorteo.java
    Actualización

--- PruebaFinal 2.0/src/Sorteo.java
+++ PruebaFinal 2.0/src/Sorteo.java
@@ -1,13 +1,34 @@
-import java.util.Collections;
-import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
-class Sorteo {
-    private List<Grupo> grupos;
+public class Sorteo {
+    public static String realizarSorteo(Partido partido) {
+        Random random = new Random();
+        String equipoLocal = partido.getEquipoLocal();
+        String equipoVisitante = partido.getEquipoVisitante();
+        int indiceEquipoGanador = random.nextInt(2); // 0 para equipo local, 1 para equipo visitante
 
-    public Sorteo(List<Grupo> grupos) {
-        this.grupos = grupos;
+        String ganador = (indiceEquipoGanador == 0) ? equipoLocal : equipoVisitante;
+        String perdedor = (ganador.equals(equipoLocal)) ? equipoVisitante : equipoLocal;
+
+        System.out.println("Usted que equipo cree que va a ganar?  (" + equipoLocal + " o " + equipoVisitante + "):");
+        Scanner scanner = new Scanner(System.in);
+        String respuesta = scanner.nextLine();
+
+        if (respuesta.equalsIgnoreCase(ganador)) {
+            return "¡Felicidades! Usted acertó. Ganó " + ganador+ " " + generarMarcadorAleatorio();
+        } else if (respuesta.equalsIgnoreCase(perdedor)) {
+            return "Lo lamento pero ganó " + perdedor + " " + generarMarcadorAleatorio();
+        } else {
+            return "No acertó. Ganó " + ganador + " " + generarMarcadorAleatorio();
+        }
     }
 
-
+    private static String generarMarcadorAleatorio() {
+        Random random = new Random();
+        int golesEquipoGanador = random.nextInt(5) + 1; // Goles entre 1 y 5
+        int golesEquipoPerdedor = random.nextInt(golesEquipoGanador); // Goles del perdedor menor a los del ganador
+        return "con un marcador de " + golesEquipoGanador + " - " + golesEquipoPerdedor;
+    }
 }
 
