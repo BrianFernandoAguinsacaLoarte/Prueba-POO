7eb5a4ea82e2bf321b3a73e851598ebbbe52425d
Brian Fernando Aguinsaca Loarte <133794609+BrianFernandoAguinsacaLoarte@users.noreply.github.com>
2023-08-12 23:50:08 -0500

    Actualización admin.py

--- Código Python/dawnbem/admin.py
+++ Código Python/dawnbem/admin.py
@@ -1,15 +1,32 @@
 from django.contrib import admin
 from dawnbem.models import Registro, Jugador, Inscripcion, Equipo, Grupo, Partido, Marcador, Temporada, Reglamento, \
-    Regla, Sorteo
+    Regla, Sorteo, RevisionInscripcion
 
-admin.site.register(Registro)
+
+class RegistroAdmin(admin.ModelAdmin):
+    search_fields = ['nombre']
+    ordering = ['nombre']
+    list_display = ('nombre', 'apellido')
+    list_filter = ('nombre',)
+    list_per_page = 10
+class InscripcionAdmin(admin.ModelAdmin):
+    list_display = ['jugador', 'EstadoInscripcion']  # Mostrar estado en la lista
+    list_filter = ['EstadoInscripcion']  # Agregar filtro por estado
+
+class ReglaAdmin(admin.ModelAdmin):
+    ordering = ['id']
+
+
+admin.site.register(Registro, RegistroAdmin)
 admin.site.register(Jugador)
-admin.site.register(Inscripcion)
+admin.site.register(Inscripcion, InscripcionAdmin)
+admin.site.register(RevisionInscripcion)
 admin.site.register(Equipo)
 admin.site.register(Grupo)
 admin.site.register(Partido)
 admin.site.register(Marcador)
 admin.site.register(Temporada)
 admin.site.register(Reglamento)
-admin.site.register(Regla)
+admin.site.register(Regla, ReglaAdmin)
 admin.site.register(Sorteo)
+
